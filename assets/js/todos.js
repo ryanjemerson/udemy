b7b9989ceffdca74e27c094bce73f790630e1718
Ryan Emerson <ryanjemerson@gmail.com>
2018-03-06 16:08:07 -0800

    194 done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,20 @@
 // Check off todos by click
-$('li').click(function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
 //Click on X to delete todo
-$('span').click(function(e){
+$("ul").on("click", "span", function(e){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+       var todoText = $(this).val();
+       $(this).val("");
+       $("ul").append("<li><span>x</span> " + todoText + "</li>");
+    }
 });
